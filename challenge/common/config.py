6f6323c83dd267948329df96af21e0b180a81208
Alejandro Uribe <jauriberamirez@gmail.com>
2026-06-12 14:39:39 -0300

    Finished Part II

--- challenge/common/config.py
+++ challenge/common/config.py
@@ -22,9 +22,6 @@ class AppSettings(BaseSettings):
     version: str
     description: str
 
-    # MLFlow
-    mlflow_tracking_uri: str
-
     # Model
     model_path: str = "model/logistic_regression.pkl"
     encoder_path: str = "model/encoder.pkl"
